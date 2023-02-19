953532dea4cf38857ffdf602f728cfb84ce7c4c3
dbrjs4594283 <dbrjs4594283@naver.com>
2023-02-19 23:31:03 +0900

    questionId 안 나오는 오류 수정

--- src/main/java/com/tecst/tecst/domain/common_question/dto/response/CommonQuestionResponseDto.java
+++ src/main/java/com/tecst/tecst/domain/common_question/dto/response/CommonQuestionResponseDto.java
@@ -2,7 +2,7 @@ package com.tecst.tecst.domain.common_question.dto.response;
 
 
 public interface CommonQuestionResponseDto {
-    Long getCommonQuestionId();
+    Long getCommon_Question_Id();
     String getContents();
     String getResponse();
 
