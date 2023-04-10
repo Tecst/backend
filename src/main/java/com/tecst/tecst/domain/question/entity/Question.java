091db3a8896d4237054a1298abbdb0d508d97803
choiseyeon <choi20014830@gmail.com>
2023-04-10 14:26:46 +0900

    feat: 개인, 기본 질문 테이블을 통합하도록 Question entity 수정

--- src/main/java/com/tecst/tecst/domain/question/entity/Question.java
+++ src/main/java/com/tecst/tecst/domain/question/entity/Question.java
@@ -1,6 +1,6 @@
-package com.tecst.tecst.domain.personalquestion.entity;
+package com.tecst.tecst.domain.question.entity;
 
-import com.tecst.tecst.domain.common_question.enumeration.Type;
+import com.tecst.tecst.global.util.Type;
 import com.tecst.tecst.domain.user.entity.User;
 import lombok.*;
 import org.hibernate.annotations.SQLDelete;
@@ -9,31 +9,37 @@ import org.hibernate.annotations.Where;
 import javax.persistence.*;
 
 
-@Entity
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-@SQLDelete(sql = "UPDATE personal_question SET is_deleted = true WHERE personal_question_id = ?")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@SQLDelete(sql = "UPDATE question SET is_deleted = true WHERE question_id = ?")
 @Where(clause = "is_deleted = false")
-public class PersonalQuestion {
+public class Question {
     @Id
     @GeneratedValue
-    private Long personalQuestionId;
+    private Long questionId;
+
+    @Column(name = "type", length = 100)
+    @Enumerated(EnumType.STRING)
+    private Type type;
+
+    @Column(name = "Response", length = 500)
+    private String response;
+
+    @Column(name = "Contents", length = 100)
+    private String content;
+
+    @Column(name = "is_deleted")
+    private Boolean isDelete = Boolean.FALSE;
 
     @ManyToOne
     @JoinColumn(name = "userId")
     private User user;
 
-    @Enumerated(EnumType.STRING)
-    private Type type;
-
-    @Column(name = "response")
-    private String response;
-
-    @Column(name = "content")
-    private String content;
-
     @Builder
-    public PersonalQuestion(User user, Type type, String response, String content) {
+    public Question(User user, Type type, String response, String content) {
         this.user = user;
         this.type = type;
         this.response = response;
