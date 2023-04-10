1259324478588069dbbc7404232ce4fe94acc730
choiseyeon <choi20014830@gmail.com>
2023-04-10 14:44:20 +0900

    feat: 도메인 병합으로 인한 파일 삭제

--- src/main/java/com/tecst/tecst/domain/personalquestion/repository/PersonalQuestionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tecst.tecst.domain.personalquestion.repository;
-
-import com.tecst.tecst.domain.personalquestion.entity.PersonalQuestion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonalQuestionRepository extends JpaRepository<PersonalQuestion, Long> {
-
-}
